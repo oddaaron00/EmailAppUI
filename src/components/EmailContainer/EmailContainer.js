c7e14cc744d9d9ff796f964b176101d474223149
oddaaron00 <caseygoodings@outlook.com>
2021-05-02 15:35:55 +0100

    Email table with correct formatting

--- src/components/EmailContainer/EmailContainer.js
+++ src/components/EmailContainer/EmailContainer.js
@@ -1,10 +1,30 @@
 import './EmailContainer.css';
 import NoEmails from "../NoEmails/NoEmails";
+import EmailSnippet from '../EmailSnippet/EmailSnippet';
 
-export default function EmailContainer({ numberOfEmails }) {
+export default function EmailContainer({ numberOfEmails, emails }) {
+    const table = (
+        <table id='emailTable'>
+            <thead id='tableHeader'>
+                <tr>
+                    <th className='senderCol' width='14%'>From</th>
+                    <th className='recipsCol' width='20%'>To</th>
+                    <th className='extraRecips' width='4%'></th>
+                    <th className='subjCol' width='47%'>Subject</th>
+                    <th className='attCol' width='4%'></th>
+                    <th className='dateCol' width='11%' id='dateHeader'>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {emails.map(email => <EmailSnippet email={email}/>)}
+            </tbody>
+        </table>
+    )
+
     return (
         <div id='emailContainer'>
             {numberOfEmails === 0 && <NoEmails/>}
+            {numberOfEmails !== 0 && table}
         </div>
     )
 }
