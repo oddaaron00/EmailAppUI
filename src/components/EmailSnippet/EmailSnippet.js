bb41a4062930d469aa80ff50d25a05e9f099260a
oddaaron00 <caseygoodings@outlook.com>
2021-05-02 17:07:37 +0100

    Add correct date formatting

--- src/components/EmailSnippet/EmailSnippet.js
+++ src/components/EmailSnippet/EmailSnippet.js
@@ -1,7 +1,7 @@
 import './EmailSnippet.css';
 import clipIcon from '../../assets/images/icon_clip.svg';
 import clipIconHover from '../../assets/images/icon_clip_hover.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 /**
  * @param {Number} email.id - ID
@@ -16,6 +16,50 @@ import { useState } from 'react';
 export default function EmailSnippet({ email }) {
     const [hover, setHover] = useState(false);
 
+    /*
+    //Function taken from https://stackoverflow.com/questions/143815/determine-if-an-html-elements-content-overflows?noredirect=1&lq=1
+    function checkOverflow(el)
+    {
+        console.log(el);
+       var curOverflow = el.style.overflow;
+    
+       if ( !curOverflow || curOverflow === "visible" )
+          el.style.overflow = "hidden";
+    
+       var isOverflowing = el.clientWidth < el.scrollWidth 
+          || el.clientHeight < el.scrollHeight;
+    
+       el.style.overflow = curOverflow;
+    
+       console.log(isOverflowing);
+    }
+
+    const isOverflown = element => {
+        let isOverflown = element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
+      }
+      
+    const fun = textArr => {
+        console.log(textArr.join(', ').scrollWidth);
+        
+    }*/
+
+    const determineDateFormat = date => {
+        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+        ];
+
+        const currentDateTime = new Date();
+        if (currentDateTime.getFullYear() === date.getFullYear() && currentDateTime.getMonth() === date.getMonth() 
+                && currentDateTime.getDate() === date.getDate()) {
+            return date.getHours() + ':' + date.getMinutes();
+        }
+        else if (currentDateTime.getFullYear() === date.getFullYear()) {
+            return monthNames[date.getMonth()] + ' ' + date.getDay();
+        } else {
+            return email.date.getFullYear() + '/' + (email.date.getMonth() + 1) + '/' + email.date.getDate();
+        }
+    }
+
     return (
         <tr className='emailSnippet' style={hover ? {color: '#0033dd'} : {}} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
             <td>{email.sender}</td>
@@ -23,7 +67,7 @@ export default function EmailSnippet({ email }) {
             <td>{}</td>
             <td>{email.subj}</td>
             <td>{typeof email.att != "undefined" && <img width='19px' src={hover ? clipIconHover : clipIcon}/>}</td>
-            <td className='dateRow'>{email.date.getFullYear()}/{email.date.getMonth() + 1}/{email.date.getDate()}</td>
+            <td className='dateRow'>{determineDateFormat(email.date)}</td>
         </tr>
     )
 }
